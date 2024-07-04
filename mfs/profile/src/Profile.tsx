229f768fa87c218879be6239d63423b36935687c
Florian Rappl <mail@florian-rappl.de>
2024-07-04 13:47:27 +0200

    Refined profile

--- mfs/profile/src/Profile.tsx
+++ mfs/profile/src/Profile.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import type { ExtensionComponentProps } from 'spa-app';
+import powerOff from './power-off.png';
 
 interface ProfileData {
   user: any;
@@ -14,11 +15,11 @@ const Profile: React.FC<ExtensionComponentProps<ProfileData>> = ({ params }) => 
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}>
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', alignItems: 'center' }}>
       <img src={user.icon} width="24" height="24" />
       <span>{user.name}</span>
-      <a href="#" onClick={exit}>
-        🚪
+      <a href="#" onClick={exit} title="Logout" style={{ padding: '0' }}>
+      <img src={powerOff} width="22" height="22" />
       </a>
     </div>
   );
